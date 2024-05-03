d9e58ffe1d0a703234455f6c2912af90916c085f
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-05-03 21:15:27 +0300

    Solution of tasks 26-30 (8kyu)

---
+++ 8kyu/Task28. Fake Binary.js
@@ -0,0 +1,17 @@
+//Task: Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+//
+// Note: input will never be an empty string
+
+
+//Solution:
+function fakeBin(x){
+    let s = ""
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] < "5") {
+            s = s + "0"
+        } else {
+            s = s + "1"
+        }
+    }
+    return s
+}
---
+++ 8kyu/Task29. ConvertABooleanToAString.js
@@ -0,0 +1,10 @@
+//Task: Implement a function which convert the given boolean value into its string representation.
+//
+// Note: Only valid inputs will be given.
+
+
+//Solution:
+function booleanToString(b){
+    return b.toString();
+    //your code here
+}
