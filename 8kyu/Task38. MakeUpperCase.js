76c0bef8009034898faeea5329726b7050e141dc
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-05-04 20:03:05 +0300

    Solution of tasks 36-40 (8kyu)

---
+++ 8kyu/Task38. MakeUpperCase.js
@@ -0,0 +1,8 @@
+//Task: Write a function which converts the input string to uppercase.
+
+
+//Solution:
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase();
+}
---
+++ 8kyu/Task39. BeginnerReduceButGrow.js
@@ -0,0 +1,9 @@
+//Task: Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+//
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+//Solution:
+function grow(x){
+    return x.reduce((acc, num) => {return acc*num}, 1)
+}
