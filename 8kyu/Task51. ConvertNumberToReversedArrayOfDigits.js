7c90fcd555acad2e88ace710f731b4e89f31d1ad
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-05-05 19:25:39 +0300

    Solution of tasks 41-55 (8kyu)

---
+++ 8kyu/Task51. ConvertNumberToReversedArrayOfDigits.js
@@ -0,0 +1,14 @@
+//Task: Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+//
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+
+//Solution:
+function digitize(n) {
+    //code here
+    let arr = Array.from(String(n), Number);
+    return arr.reverse();
+}
