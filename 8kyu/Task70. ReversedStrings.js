0cc0e23eaf2ef97be9ee6f6b12642bce15ceda2e
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-05-05 19:35:25 +0300

    Solution of tasks 66-70 (8kyu)

---
+++ 8kyu/Task70. ReversedStrings.js
@@ -0,0 +1,16 @@
+//Task: Complete the solution so that it reverses the string passed into it.
+//
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+//Solution:
+function solution(str){
+
+    return str.split("").reverse().join("");
+
+//   let strSplit = str.split("");
+//   let reverseArr = strSplit.reverse();
+//   let joinStr = reverseArr.join("");
+//   return joinStr;
+}
