e3fca2f902309acfca4bc236e4a82728fbb7a47d
Kirill190611 <k.igorevitch88@yandex.ru>
2024-07-24 22:34:16 +0300

    Task solution for tasks 50-51 (7kyu)

---
+++ 7kyu/Task50. SumOfAngles.js
@@ -0,0 +1,8 @@
+//Task: Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+
+//Solution:
+function angle(n) {
+    //code here
+    return (n - 2) * 180;
+}
