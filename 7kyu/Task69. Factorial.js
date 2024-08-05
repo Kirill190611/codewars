e7e11d8208988bcd7cee958c1adfeedcb276c5dd
Kirill190611 <k.igorevitch88@yandex.ru>
2024-08-05 20:45:43 +0300

    Task solution for tasks 69-71 (7kyu)

---
+++ 7kyu/Task69. Factorial.js
@@ -0,0 +1,11 @@
+//Task: Your task is to write function factorial.
+
+
+//Solution:
+function factorial(n){
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result = result * i
+    }
+    return result;
+}
