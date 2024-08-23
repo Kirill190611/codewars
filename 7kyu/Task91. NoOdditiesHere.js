9265baf9a7d92804db88e3f6ae0956ce14e45491
Kirill190611 <k.igorevitch88@yandex.ru>
2024-08-23 20:15:53 +0300

    Task solution for tasks 90-95 (7kyu)

---
+++ 7kyu/Task91. NoOdditiesHere.js
@@ -0,0 +1,17 @@
+//Task: Write a small function that returns the values of an array that are not odd.
+//
+// All values in the array will be integers. Return the good values in the order they are given.
+
+
+//Solution:
+function noOdds( values ){
+    /*let result = [];
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] % 2 === 0) {
+            result.push(values[i])
+        }
+    }
+    return result*/
+
+    return values.filter(el => el % 2 === 0);
+}
