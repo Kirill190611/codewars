0af6dea923f272cc216c6890d9ec14c9f8f106d0
Kirill190611 <k.igorevitch88@yandex.ru>
2024-10-27 19:41:59 +0300

    Task solution for task 50 (6 kyu)

--- 6kyu/Task50. TitleCase.js
+++ 6kyu/Task50. TitleCase.js
@@ -18,7 +18,7 @@
 function titleCase(title, minorWords) {
     minorWords = typeof minorWords !== "undefined" ? minorWords.toLowerCase().split(' ') : [];
     return title.toLowerCase().split(' ').map(function(v, i) {
-        if(v != "" && ( (minorWords.indexOf(v) === -1) || i == 0)) {
+        if(v !== "" && ( (minorWords.indexOf(v) === -1) || i === 0)) {
             v = v.split('');
             v[0] = v[0].toUpperCase();
             v = v.join('');
