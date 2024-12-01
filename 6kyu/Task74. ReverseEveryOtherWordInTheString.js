c2f1d3fa2023f152cf367c617835db37c490eda6
Kirill190611 <k.igorevitch88@yandex.ru>
2024-12-01 22:59:06 +0300

    Solution for task 74 (6 kyu)

--- 6kyu/Task74. ReverseEveryOtherWordInTheString.js
+++ 6kyu/Task74. ReverseEveryOtherWordInTheString.js
@@ -1,4 +1,13 @@
 //Task: Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
-// https://www.codewars.com/kata/58d76854024c72c3e20000de
+
 
 //Solution:
+function reverse(str){
+    return str.split(" ").map((el, index) => {
+        return index % 2 !== 0
+            ? el.split("")
+                .reverse()
+                .join("")
+            : el
+    }).join(" ").trim()
+}
