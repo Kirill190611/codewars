1839365e8466b5c57742c92b739a815249927dc3
Kirill190611 <k.igorevitch88@yandex.ru>
2024-12-05 21:42:34 +0300

    Solution for task 75 (6 kyu)

---
+++ 6kyu/Task75. DashatizeIt.js
@@ -0,0 +1,10 @@
+//Task: Given an integer, return a string with dash '-' marks before and after each odd digit, but do not begin or end the string with a dash mark.
+//
+// Ex:
+//
+// 274 -> '2-7-4'
+// 6815 -> '68-1-5'
+
+
+//Solution:
+const dashatize = (num) => String(num).replace(/([13579])/g, "-$1-").replace(/--+/g, "-").replace(/(^-|-$)/g, "");
