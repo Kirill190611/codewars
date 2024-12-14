e1004bc7e77e7d26505eaf33bb4cca78c161f69b
Kirill190611 <k.igorevitch88@yandex.ru>
2024-12-14 20:29:38 +0300

    Solution for task 81 (6 kyu)

---
+++ 6kyu/Task81.StringArrayDuplicates.js
@@ -0,0 +1,33 @@
+//Task: In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+//
+// For example:
+//
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+//
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+//
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+//
+// Good luck!
+//
+// If you like this Kata, please try:
+//
+// Alternate capitalization
+//
+// Vowel consonant lexicon
+
+
+//Solution:
+function dup(s) {
+    const filterStr = (str) => {
+        const arrOfChar = str.split('')
+        const arrayOfNonRepChar = arrOfChar.filter((char, i, arrOfChar) => {
+            return char !== arrOfChar[i-1]
+        })
+        return arrayOfNonRepChar.join('')
+    }
+
+    return s.map(string => {
+        return filterStr(string)
+    })
+};
