f8180541abf2c7bb13e054b3d24703d87ac74689
Kirill190611 <k.igorevitch88@yandex.ru>
2025-01-03 17:50:38 +0300

    Solution for task 13 (5 kyu)

---
+++ 5kyu/Task13. ExtractTheDomainNameFromAUrl.js
@@ -0,0 +1,23 @@
+//Task: Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+//
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+
+//Solution:
+const w = 'www.'
+const s = '//'
+
+const getDomain = (url, string) => {
+    const startedDomain = url.indexOf(string) + string.length
+    url = url.substring(startedDomain)
+    return url.split('.')[0]
+}
+
+const domainName = (url) => {
+    if (url.includes(w)) return getDomain(url, w)
+    if (url.includes(s)) return getDomain(url, s)
+
+    return url.split('.')[0]
+}
