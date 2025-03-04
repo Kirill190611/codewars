d5c75445588b5f2acd314e41de69685256a7c2db
Kirill190611 <k.igorevitch88@yandex.ru>
2025-03-04 22:40:20 +0300

    Solution for tasks 26-30 (8kyu)

---
+++ 8kyu/Tasks/27. Beginner - Lost Without a Map.js
@@ -0,0 +1,8 @@
+//T: Given an array of integers, return a new array with each value doubled.
+//
+// For example:
+//
+// [1, 2, 3] --> [2, 4, 6]
+
+//S:
+const maps = x => x.map(el => el * 2)
---
+++ 8kyu/Tasks/30. Convert a Boolean to a String.js
@@ -0,0 +1,6 @@
+//T: Implement a function which convert the given boolean value into its string representation.
+//
+// Note: Only valid inputs will be given.
+
+//S:
+const booleanToString = b => String(b)
