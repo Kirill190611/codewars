d56e964d67a6bbdc440518def803b98876d203bc
Kirill190611 <k.igorevitch88@yandex.ru>
2025-03-05 21:24:02 +0300

    Solution for tasks 34-36 (8kyu)

---
+++ 8kyu/Tasks/36. Simple multiplication.js
@@ -0,0 +1,4 @@
+//T: This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+//S:
+const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9
