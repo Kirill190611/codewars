168499aaf8d47e396362b76edfaf696688432580
Kirill190611 <k.igorevitch88@yandex.ru>
2025-03-05 21:39:32 +0300

    Solution for tasks 40-42 (8kyu)

---
+++ 8kyu/Tasks/40. Fake Binary.js
@@ -0,0 +1,6 @@
+//T: Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+//
+// Note: input will never be an empty string
+
+//S:
+const fakeBin = x => [...x].map(el => el < 5 ? '0' : '1').join('')
---
+++ 8kyu/Tasks/42. Beginner - Reduce but Grow.js
@@ -0,0 +1,6 @@
+//T: Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+//
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+//S:
+const grow = x => x.reduce((acc, el) => acc * el, 1)
