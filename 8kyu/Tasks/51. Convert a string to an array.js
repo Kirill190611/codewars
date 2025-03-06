b9369d12e871711b341eb08559b7fb26403b7e52
Kirill190611 <k.igorevitch88@yandex.ru>
2025-03-06 20:32:40 +0300

    Solution for tasks 49-51 (8kyu)

---
+++ 8kyu/Tasks/51. Convert a string to an array.js
@@ -0,0 +1,7 @@
+//T: Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+//
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+//S:
+const stringToArray = string => string.split(' ')
