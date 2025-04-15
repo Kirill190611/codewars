f9dd3c68275a7f10262ad13a57e3ca80c25dde62
Kirill190611 <k.igorevitch88@yandex.ru>
2025-04-15 19:05:34 +0300

    Solution for tasks 115 (7kyu)

--- 7kyu/Tasks/115. Sum of numbers from 0 to N.js
+++ 7kyu/Tasks/115. Sum of numbers from 0 to N.js
@@ -30,12 +30,12 @@ var SequenceSum = (function() {
 
     SequenceSum.showSequence = function(count) {
         if (count < 0) return count + "<" + 0;
-        if (count == 0) return count + "=" + 0;
+        if (count === 0) return count + "=" + 0;
 
         let sum = 0, str = "";
         for (let i = 0; i <= count; i++) {
             sum += count;
-            str += i + ((i != count) ? "+" : " = " + sum / 2);
+            str += i + ((i !== count) ? "+" : " = " + sum / 2);
         }
 
         return str;
