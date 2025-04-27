1787acc1fa8b91f99441cb41333500e2957f4283
Kirill190611 <k.igorevitch88@yandex.ru>
2025-04-27 14:27:12 +0300

    Solution for tasks 131 (7kyu)

---
+++ 7kyu/Tasks/131. Smallest value of an array.js
@@ -0,0 +1,15 @@
+//Task: Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+//
+// Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+//
+// Some examples:
+//
+// ([1,2,3,4,5], "value") should return 1
+// ([1,2,3,4,5], "index") should return 0
+
+//Solution:
+const min1 = (arr, toReturn) => {
+    const minValue = [...arr].sort((a, b) => a - b)[0]
+    return toReturn === 'value' ? minValue : arr.indexOf(minValue)
+}
+const min = (arr, toReturn) => toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr))
