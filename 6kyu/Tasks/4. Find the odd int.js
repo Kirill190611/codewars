4d6ded114b2453f3f280ad08418c7d2f6b569d9d
Kirill190611 <k.igorevitch88@yandex.ru>
2025-05-09 13:30:44 +0300

    Solution for tasks 4 (6kyu)

---
+++ 6kyu/Tasks/4. Find the odd int.js
@@ -0,0 +1,26 @@
+//Task: Description:
+// Given an array of integers, find the one that appears an odd number of times.
+//
+// There will always be only one integer that appears an odd number of times.
+//
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+//Solution:
+const findOdd = A => {
+    const data = A.reduce((acc, el) => {
+        if (acc[el]) {
+            acc[el] = acc[el] + 1
+        } else {
+            acc[el] = 1
+        }
+        return acc
+    }, {})
+
+    const result = Object.entries(data).filter(([_, r]) => r % 2 !== 0)
+    return Number(result[0][0])
+}
