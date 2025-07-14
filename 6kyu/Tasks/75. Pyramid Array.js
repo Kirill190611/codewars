bd2d649600b0dd564771539f36acc90b914bf77e
Kirill190611 <k.igorevitch88@yandex.ru>
2025-07-14 22:36:32 +0300

    Solution for tasks 75 (6kyu)

--- 6kyu/Tasks/75. Pyramid Array.js
+++ 6kyu/Tasks/75. Pyramid Array.js
@@ -21,8 +21,8 @@ const pyramid = n => {
 
 const pyramid1 = n => {
     const result = [];
-    for(let i = 0; i < n; i++){
-        result.push([...Array(i+1)].fill(1))
+    for (let i = 0; i < n; i++) {
+        result.push([...Array(i + 1)].fill(1))
     }
     return result;
 }
