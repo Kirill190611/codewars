0dd2715e6cafa370ee9fa2a470bd1af71766875f
Kirill190611 <k.igorevitch88@yandex.ru>
2025-07-19 22:59:51 +0300

    Solution for tasks 80 (6kyu)

---
+++ 6kyu/Tasks/80. Reverse every other word in the string.js
@@ -0,0 +1,12 @@
+//Task: Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+//Solution:
+const reverse = str => {
+    return str.split(" ").map((el, index) => {
+        return index % 2 !== 0
+            ? [...el]
+                .reverse()
+                .join("")
+            : el
+    }).join(" ").trim()
+}
