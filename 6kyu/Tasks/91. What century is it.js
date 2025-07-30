10326e9ed3ce42176b78a6ef51cc3974023a81fe
Kirill190611 <k.igorevitch88@yandex.ru>
2025-07-30 22:36:19 +0300

    Solution for tasks 91 (6kyu)

---
+++ 6kyu/Tasks/91. What century is it.js
@@ -0,0 +1,26 @@
+//Task: Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation.
+//
+// Examples
+// "1999" --> "20th"
+// "2011" --> "21st"
+// "2154" --> "22nd"
+// "2259" --> "23rd"
+// "1124" --> "12th"
+// "2000" --> "20th"
+
+//Solution:
+const whatCentury = year => {
+    const century = Math.ceil(Number(year) / 100)
+    const suffix =
+        century === 11 || century === 12 || century === 13
+            ? 'th'
+            : century % 10 === 1
+                ? 'st'
+                : century % 10 === 2
+                    ? 'nd'
+                    : century % 10 === 3
+                        ? 'rd'
+                        : 'th'
+
+    return `${century}${suffix}`
+}
