bb9e6c8f5a965ea76af994e181845b7d30131470
Kirill190611 <k.igorevitch88@yandex.ru>
2025-08-06 22:45:37 +0300

    Solution for tasks 97 (6kyu)

---
+++ 6kyu/Tasks/97. Kebabize.js
@@ -0,0 +1,11 @@
+//Task: Modify the kebabize function so that it converts a camel case string into a kebab case.
+//
+// "camelsHaveThreeHumps"  -->  "camels-have-three-humps"
+// "camelsHave3Humps"  -->  "camels-have-humps"
+// "CAMEL"  -->  "c-a-m-e-l"
+// Notes:
+//
+// the returned string should only contain lowercase letters
+
+//Solution:
+const kebabize = (str) => str.replace(/([A-Z])/g, "-$1").replace(/[0-9]/g, '').toLowerCase().replace(/^-/, '');
