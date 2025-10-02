76373da3d18da337f66ecd9d910e360aac4414b1
Kirill190611 <k.igorevitch88@yandex.ru>
2025-10-02 21:56:03 +0300

    Solution for tasks 127 (6kyu)

---
+++ 6kyu/Tasks/127. Twisted Sum.js
@@ -0,0 +1,28 @@
+//Task: Find the sum of the digits of all the numbers from 1 to N (both ends included).
+//
+// Examples
+// # N = 4
+// 1 + 2 + 3 + 4 = 10
+//
+// # N = 10
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) = 46
+//
+// # N = 12
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
+
+//Solution:
+const twistedSum = n => {
+    let sum = 0
+    for (let i = 0; i <= n; i++) {
+        let numStr = i.toString()
+        if (numStr.length === 1) {
+            sum = sum + (+numStr)
+        } else {
+            let subArr = numStr.split('')
+            subArr.forEach(el => {
+                sum = sum + (+el)
+            })
+        }
+    }
+    return sum
+}
