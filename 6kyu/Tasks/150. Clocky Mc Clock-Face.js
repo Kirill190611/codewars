ad46754cb4210408cbdd6a7fed4c6e7524d8aca3
Kirill190611 <k.igorevitch88@yandex.ru>
2025-10-26 19:46:09 +0300

    Solution for tasks 151 and code optimization for task 150 (6kyu)

--- 6kyu/Tasks/150. Clocky Mc Clock-Face.js
+++ 6kyu/Tasks/150. Clocky Mc Clock-Face.js
@@ -33,12 +33,18 @@
 // 7 minutes past noon is also 12:07
 
 //Solution:
-var whatTimeIsIt = function(angle) {
-    mins = angle*2;
-    h = Math.floor(mins / 60);
-    m = Math.floor(mins % 60);
-    if (h==0) {h = "12"}
-    if (h<10) {h = "0"+h}
-    if (m<10) {m = "0"+m}
-    return h + ":" + m;
+const whatTimeIsIt = angle => {
+    let mins = angle * 2
+    let h = Math.floor(mins / 60)
+    let m = Math.floor(mins % 60)
+    if (h === 0) {
+        h = "12"
+    }
+    if (h < 10) {
+        h = "0" + h
+    }
+    if (m < 10) {
+        m = "0" + m
+    }
+    return h + ":" + m
 }
