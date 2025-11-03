ba674d757e5efd508285725b4ae6708168a8c167
Kirill190611 <k.igorevitch88@yandex.ru>
2025-11-03 11:03:23 +0300

    Solution for tasks 159 (6kyu)

---
+++ 6kyu/Tasks/159. N-th Fibonacci.js
@@ -0,0 +1,19 @@
+//Task: I love Fibonacci numbers in general, but I must admit I love some more than others.
+//
+// I would like for you to write me a function that, when given a number n (n >= 1 ), returns the nth number in the Fibonacci Sequence.
+//
+// For example:
+//
+//    nthFibo(4) == 2
+// Because 2 is the 4th number in the Fibonacci Sequence.
+//
+// For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+
+//Solution:
+const nthFibo = n => {
+    let arr = [0, 1]
+    for (let i = 2; i <= n; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1]
+    }
+    return arr[n - 1] || 0
+}
